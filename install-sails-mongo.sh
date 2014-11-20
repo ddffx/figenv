23648bb3401b199a8834d28e2719d676348dd3dc
Das, Deb <Debabrata.Das@washpost.com>
2014-11-19 19:25:29 -0500

    Update mongo adapter script

--- install-sails-mongo.sh
+++ install-sails-mongo.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 set -ex
+echo 'Install sails-mongo'
 
-echo 'install sails mongo adapter'
-
-# got to app directory
-cd testapp
+if [ "$1" != "" ]; then
+	echo "goto app directory "$1
+	cd $1
+else 
+	echo 'no args, goto app directory testapp'
+	cd testapp
+fi
 
 npm install sails-mongo --save
